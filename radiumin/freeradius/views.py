9f74661901bdcef90a1244476a98400dd29e8a37
jahnito <jahn@pc.h>
2026-05-20 22:36:46 +0500

    Add templates

--- radiumin/freeradius/views.py
+++ radiumin/freeradius/views.py
@@ -1,7 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+
+from .models import RadcheckModel, RadusergroupModel
+from .forms import RadiusLogin
 
 
 def index(request):
-    context = {'name': 'jahn'}
-    return render(request, 'freeradius/index.html', context=context)
+    def login(request):
+        form = RadiusLogin()
+        return render(request, 'freeradius/index.html', context={'form': form})
+
+    if request.method == 'POST':
+        form = RadiusLogin(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = RadcheckModel.objects.filter(
+                rad_username=username,
+                value=password).first()
+            if user is not None:
+                groups = RadusergroupModel.objects.filter(
+                    rad_username=user.rad_username
+                )
+                context = {
+                    'user': user,
+                    'groups': groups,
+                }
+                return render(
+                    request, 'freeradius/user.html', context=context
+                )
+            else:
+                return login(request)
+        else:
+            return login(request)
+    else:
+        return login(request)
